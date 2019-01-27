72814b4c1f9cfaa3adeacf8e00338aad9980271e
ArcherTester1220 <5881973@qq.com>
2019-01-27 19:11:25 +0800

    update

--- src/main/java/com/archer/DemoApplication.java
+++ src/main/java/com/archer/DemoApplication.java
@@ -1,9 +1,13 @@
-package com.archer.demo;
+package com.archer;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.archer.ats.mapper")
+@EnableAspectJAutoProxy
 public class DemoApplication {
 
     public static void main(String[] args) {
